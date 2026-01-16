32300e1f9cc457b35b53cd16e972a3ffa83271ea
yasza <86324593+yaszaxz@users.noreply.github.com>
2026-01-16 11:06:27 -0300

    Código sem a apikey (usar mensagem fixa pois não tem crédito na openai)

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # jornadapython-chatbot
 Chatbot feito seguindo as orientações da quarta aula da Jornada Python da Hashtag Programação
+
+Ferramenta usada: Streamlit
+IA usada: OpenAI
---
+++ main.py
@@ -0,0 +1,42 @@
+import streamlit as st
+#Importar somente a integração da api da OpenAI
+from openai import OpenAI
+
+#Definir o modelo da IA
+modeloIA = OpenAI(api_key='')
+
+#Título da página
+st.write('# IA foda')
+
+#Condicional para criar a lista de mensagem apenas uma vez
+#Lista para salvar as mensagens
+if not 'listaMensagens' in st.session_state:
+    st.session_state['listaMensagens'] = []
+
+#Campo de chat com placeholder
+textoUsuario = st.chat_input('Digite a sua mensagem...')
+
+#Loop para exibir as mensagens na tela
+for mensagem in st.session_state['listaMensagens']:
+    #Definir quem mandou a mensagem e qual é a mensagem
+    role = mensagem['role']
+    content = mensagem['content']
+    st.chat_message(role).write(content)
+
+
+#Condicional para não ficar exibindo 'none' na mensagem
+if (textoUsuario):
+    #Mensagens do usuário
+    st.chat_message('user').write(textoUsuario)
+    mensagemUsuario = {'role': 'user', 'content': textoUsuario}
+    st.session_state['listaMensagens'].append(mensagemUsuario)
+
+    #Mensagens da IA (deixar uma resposta fixa pq não tem crédito)
+    resposta = 'Você disse: ' + textoUsuario
+    st.session_state['listaMensagens'].append(resposta)
+    st.chat_message('assistant').write(resposta)
+    #resposta = modeloIA.chat.completions.create(messages=st.session_state['listaMensagens'], model='gpt-4o')    
+    #textoResposta = resposta.choices[0].message.content
+    #st.chat_message('assistant').write(textoResposta)
+    #mensagemIa = {'role': 'assistant', 'content': textoResposta}
+    #st.session_state['listaMensagens'].append(mensagemIa)
